389d388ee0a29573c66c418f69a6693a8637cf69
Mateus S. Araujo <mateus0santiago@proton.me>
2026-03-08 21:30:57 -0300

    volume

--- sway/status.sh
+++ sway/status.sh
@@ -3,7 +3,8 @@
 while true; do
   battery="$(cat /sys/class/power_supply/BAT1/capacity) $(cat /sys/class/power_supply/BAT1/status)"
   date="$(date)"
+  volume="$(wpctl get-volume @DEFAULT_SINK@)"
 
-  echo "$battery | $date"
+  echo "$volume | $battery | $date"
   sleep 1
 done
